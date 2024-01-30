8052d8a4d2ac6c79c49b35ac4eeafe111173d9f8
Kyle Glasper <ksglasper@gmail.com>
2024-01-30 17:37:25 -0600

    more datascraping done

--- more_practice.py
+++ more_practice.py
@@ -1,12 +1,10 @@
-# import codecademylib3_seaborn
+from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-import numpy as np
-from bs4 import BeautifulSoup
-import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 
-matplotlib.use('Agg')
+
 prefix = "https://content.codecademy.com/courses/beautifulsoup/"
 webpage_response = requests.get('https://content.codecademy.com/courses/beautifulsoup/cacao/index.html')
 
@@ -18,13 +16,26 @@ chocolate_ratings = soup.find_all(attrs={"class": "Rating"})
 
 ratings = []
 
-for rating in chocolate_ratings:
-    if(rating.get_text() == 'Rating'):
-        continue
+for rating in chocolate_ratings[1:]:
     ratings.append(float(rating.get_text()))
 
-
-# print(ratings)
-    
 plt.hist(ratings)
 plt.show()
+
+# print(ratings)
+
+names = []
+company_names = soup.select(".Company")
+# print(company_names)
+for name in company_names[1:]:
+    names.append(name.get_text())
+
+# print(names)
+    
+d = {"Company" : names, "Ratings" : ratings}
+chocolate_df = pd.DataFrame.from_dict(d)
+print(chocolate_df.nlargest(4, 'Ratings', keep='all'))
+average_rating = chocolate_df.groupby(['Company']).Ratings.mean()
+top_ten = average_rating.nlargest(10)
+print(top_ten)
+

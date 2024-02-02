a452524674f166835a3671569baa6d3480f8f5a2
Kyle Glasper <ksglasper@gmail.com>
2024-02-01 22:05:31 -0600

    last edit for the day

--- more_practice.py
+++ more_practice.py
@@ -17,43 +17,43 @@ def string_in_list(list, string):
 
 
 ######################## This section scrapes all drafting data by year ############
-for year in range(14, 24):
-  league_link = "https://fantasy.nfl.com/league/2338570/history/20" + str(year) + "/draftresults?draftResultsDetail=0&draftResultsTab=round&draftResultsType=results"
+# for year in range(14, 24):
+#   league_link = "https://fantasy.nfl.com/league/2338570/history/20" + str(year) + "/draftresults?draftResultsDetail=0&draftResultsTab=round&draftResultsType=results"
   
-  nfl_webpage_response = requests.get(league_link)
-  nfl_page = nfl_webpage_response.content
-  nfl_soup = BeautifulSoup(nfl_page, "html.parser")
+#   nfl_webpage_response = requests.get(league_link)
+#   nfl_page = nfl_webpage_response.content
+#   nfl_soup = BeautifulSoup(nfl_page, "html.parser")
 
-  nfl_draft_order = nfl_soup.find_all(attrs={"class": "count"})
-  nfl_draft_players = nfl_soup.find_all(attrs={"class": "playerName"})
-  team_name = nfl_soup.find_all("li", class_="first last")
-  nfl_draft_auction_for = nfl_soup.find_all(attrs={"class": "auctionCost"})
-  draft_order = []
-  auction_amount = []
-  player_names = []
-  team_names = []
-  for order in nfl_draft_order:
-    order_to_number = order.get_text().split('.')[0]
-    draft_order.append(int(order_to_number))
-  for price in nfl_draft_auction_for:
-    auction_to_number = price.get_text().split('$')[1]
-    auction_amount.append(int(auction_to_number))
+#   nfl_draft_order = nfl_soup.find_all(attrs={"class": "count"})
+#   nfl_draft_players = nfl_soup.find_all(attrs={"class": "playerName"})
+#   team_name = nfl_soup.find_all("li", class_="first last")
+#   nfl_draft_auction_for = nfl_soup.find_all(attrs={"class": "auctionCost"})
+#   draft_order = []
+#   auction_amount = []
+#   player_names = []
+#   team_names = []
+#   for order in nfl_draft_order:
+#     order_to_number = order.get_text().split('.')[0]
+#     draft_order.append(int(order_to_number))
+#   for price in nfl_draft_auction_for:
+#     auction_to_number = price.get_text().split('$')[1]
+#     auction_amount.append(int(auction_to_number))
 
-  for name in nfl_draft_players:
-    player_names.append(name.get_text())
+#   for name in nfl_draft_players:
+#     player_names.append(name.get_text())
 
-  for name in team_name:
-    team_names.append(name.get_text())
+#   for name in team_name:
+#     team_names.append(name.get_text())
   
-  if len(auction_amount) > 0:
-    draft_2014_dict = {"Draft Number" : draft_order, "Player Name" : player_names, "Auction Amount": auction_amount, "Team Name": team_name }
-    draft_2014 = pd.DataFrame.from_dict(draft_2014_dict)
-  else:
-    draft_2014_dict = {"Draft Number" : draft_order, "Player Name" : player_names, "Team Name": team_name }
-    draft_2014 = pd.DataFrame.from_dict(draft_2014_dict)
-    
-  print("Year: 20" + str(year))
-  print(draft_2014)
+#   if len(auction_amount) > 0:
+#     draft_2014_dict = {"Draft Number" : draft_order, "Player Name" : player_names, "Auction Amount": auction_amount, "Team Name": team_name }
+#     draft_2014 = pd.DataFrame.from_dict(draft_2014_dict)
+#   else:
+#     draft_2014_dict = {"Draft Number" : draft_order, "Player Name" : player_names, "Team Name": team_name }
+#     draft_2014 = pd.DataFrame.from_dict(draft_2014_dict)
+
+#   print("Year: 20" + str(year))
+#   print(draft_2014)
 
 ######################## This section scrapes all Win/Loss data by year ############
 for year in range(14, 24):
@@ -87,9 +87,8 @@ for year in range(14, 24):
     cycle_count +=1
 
   team_to_player_dict = dict(zip(team_name, user_name))
-#   print(team_to_player_dict)
 
-  #Create DataFrame with Player, Moves, and Trades by year
+  ##Create DataFrame with Player, Moves, and Trades by year
   move_trade_dict = {"Name" : user_name, "Moves" : moves, "Trades" : trades}
   name_moves_trade_df = pd.DataFrame.from_dict(move_trade_dict)
   print("Year: 20" + str(year))
